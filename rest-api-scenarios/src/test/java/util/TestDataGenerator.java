8dceaa39652d87b3e8eb23d1fda0a354936f6be6
Ghada Ayari <gayari@iobeya.com>
2022-05-31 01:03:16 +0100

    add test data generator

---
+++ rest-api-scenarios/src/test/java/util/TestDataGenerator.java
@@ -0,0 +1,14 @@
+package util;
+
+import java.util.UUID;
+
+public class TestDataGenerator {
+    public static String uuid() {
+        return UUID.randomUUID().toString();
+    }
+
+    public static int randomNumber(int min, int max) {
+        return (int) (Math.random() * (max - min + 1) + min);
+    }
+
+}
